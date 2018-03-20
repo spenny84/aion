043016cdc36ca8ee587f7f19e699bc2fddb11bf5
aion-Ross <rossk123@gmail.com>
2018-03-20 16:34:47 -0400

    Working

--- modAionImpl/test/org/aion/zero/impl/valid/EquihashSolutionRuleTest.java
+++ modAionImpl/test/org/aion/zero/impl/valid/EquihashSolutionRuleTest.java
@@ -34,21 +34,14 @@
  ******************************************************************************/
 package org.aion.zero.impl.valid;
 
-import org.aion.base.util.ByteArrayWrapper;
 import org.aion.equihash.EquiUtils;
-import org.aion.equihash.EquiValidator;
 import org.aion.equihash.Equihash;
-import org.aion.equihash.OptimizedEquiValidator;
-import org.aion.zero.impl.valid.EquihashSolutionRule;
 import org.aion.zero.types.A0BlockHeader;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.MockitoAnnotations;
 
 import java.math.BigInteger;
-
-import static com.google.common.truth.Truth.assertThat;
-import static org.junit.Assert.assertThat;
 
 public class EquihashSolutionRuleTest {
     @Before
@@ -70,7 +63,7 @@ public class EquihashSolutionRuleTest {
         byte[] nonceBytes = new byte[32];
 
         System.arraycopy(unpaddedNonceBytes, 0, nonceBytes, 32 - unpaddedNonceBytes.length, unpaddedNonceBytes.length);
-        System.out.println(new ByteArrayWrapper(nonceBytes));
+        //System.out.println(new ByteArrayWrapper(nonceBytes));
 
         A0BlockHeader header = new A0BlockHeader.Builder().build();
         header.setNonce(nonceBytes);
