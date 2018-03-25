aadee026fb7c89decda08a050fc986c658585d21
qoire <mr.sun.yao@gmail.com>
2018-03-25 14:52:54 -0400

    updates to rules

--- modMcf/src/org/aion/mcf/valid/AbstractBlockValidator.java
+++ modMcf/src/org/aion/mcf/valid/AbstractBlockValidator.java
@@ -5,9 +5,8 @@ import org.aion.mcf.blockchain.IBlockConstants;
 import org.aion.mcf.types.AbstractBlock;
 import org.aion.mcf.types.AbstractTransaction;
 
-public class AbstractBlockValidator<TX extends AbstractTransaction, BH extends IBlockHeader> {
+public abstract class AbstractBlockValidator<TX extends AbstractTransaction, BH extends IBlockHeader> {
     protected IBlockConstants blockConstants;
-
 
     public AbstractBlockValidator(IBlockConstants blockConstants) {
         this.blockConstants = blockConstants;
@@ -17,21 +16,74 @@ public class AbstractBlockValidator<TX extends AbstractTransaction, BH extends I
         ValidateResult result = new ValidateResult();
         IBlockHeader header = block.getHeader();
 
-//        if (!validateHashLengthRule(header.getCoinbase().toBytes()))
-//            result.addViolatedRule(COINBASE_RULE, null);
-//
-//        if (!validateHashLengthRule(header.getStateRoot())) {
-//            result.addViolatedRule();
-//        }
+        if (!validateHashLengthRule(header.getCoinbase().toBytes()))
+            result.addViolatedRule(RuleSet.COINBASE_LENGTH_RULE, null);
+
+        if (!validateHashLengthRule(header.getStateRoot())) {
+            result.addViolatedRule(RuleSet.STATE_ROOT_LENGTH_RULE, null);
+        }
+
+        if (!validateHashLengthRule(header.getTxTrieRoot())) {
+            result.addViolatedRule(RuleSet.TX_TRIE_LENGTH_RULE, null);
+        }
+
+        if (!validateHashLengthRule(header.getReceiptsRoot())) {
+            result.addViolatedRule(RuleSet.RECEIPT_TRIE_LENGTH_RULE, null);
+        }
+
+        if (!validateLogsBloomLengthRule(header.getLogsBloom())) {
+            result.addViolatedRule(RuleSet.LOGS_BLOOM_LENGTH_RULE, null);
+        }
+
+        // TODO: missing number length rule
+        // TODO: missing timestamp length rule
+
+        if (!validateHashMaxLengthRule(header.getExtraData())) {
+            result.addViolatedRule(RuleSet.EXTRA_DATA_MAX_LENGTH_RULE, null);
+        }
+
+        if (!validateHashMaxLengthRule(header.getNonce())) {
+            result.addViolatedRule(RuleSet.EXTRA_DATA_MAX_LENGTH_RULE, null);
+        }
+
+        // TODO: missing energy consumed rule
+        // TODO: missing energy limit rule
+
+        return result;
+    }
+
+    public ValidateResult validateIndependantRule(AbstractBlock<BH, TX> block) {
         return null;
     }
 
     // define hash to be a byte array of length 32 that exists
     protected static boolean validateHashLengthRule(byte[] coinbase) {
-        if (coinbase == null)
+        return validateLength(coinbase, 32);
+    }
+
+    protected static boolean validateLogsBloomLengthRule(byte[] logsBloom) {
+        return validateLength(logsBloom, 2048);
+    }
+
+    protected static boolean validateLength(byte[] input, int len) {
+        if (input == null)
             return false;
 
-        if (coinbase.length != 32)
+        if (input.length != len)
+            return false;
+        return true;
+    }
+
+
+    protected static boolean validateHashMaxLengthRule(byte[] input) {
+        return validateMaxLength(input, 32);
+    }
+
+    protected static boolean validateMaxLength(byte[] input, int len) {
+        if (input == null)
+            return false;
+
+        if (input.length > len)
             return false;
         return true;
     }
--- modMcf/src/org/aion/mcf/valid/RuleSet.java
+++ modMcf/src/org/aion/mcf/valid/RuleSet.java
@@ -8,40 +8,66 @@ public class RuleSet {
     // this is technically rule 0
     private static final String UNKNOWN_RULE = "UnknownViolation";
 
+    public static final int COINBASE_LENGTH_RULE            = 1;
+    public static final int STATE_ROOT_LENGTH_RULE          = 2;
+    public static final int TX_TRIE_LENGTH_RULE             = 3;
+    public static final int RECEIPT_TRIE_LENGTH_RULE        = 4;
+    public static final int LOGS_BLOOM_LENGTH_RULE          = 5;
+    public static final int NUMBER_MAX_SIZE_RULE            = 6;
+    public static final int TIMESTAMP_MAX_SIZE_RULE         = 7;
+    public static final int EXTRA_DATA_MAX_LENGTH_RULE      = 8;
+    public static final int NONCE_MAX_SIZE_RULE             = 9;
+    public static final int ENERGY_CONSUMED_MAX_SIZE_RULE   = 10;
+    public static final int ENERGY_LIMIT_MAX_SIZE_RULE      = 11;
+
+    // base independant validation rules (100)
+    public static final int POW_DIFFICULTY_RULE             = 100;
+    public static final int ENERGY_CONSUMED_LIMIT_RULE      = 101;
+
+    // start dependant validation rules (200)
+    public static final int BLOCK_NUMBER_RULE               = 200;
+    public static final int ENERGY_LIMIT_RULE               = 201;
+    public static final int TIMESTAMP_RULE                  = 202;
+
+    // start post-execution dependant validation rules (300)
+    public static final int ENERGY_MATCH_RULE               = 300;
+    public static final int RECEIPT_HASH_MATCH_RULE         = 301;
+    public static final int LOGS_BLOOM_MATCH_RULE           = 302;
+    public static final int WORLD_STATE_MATCH_RULE          = 303;
+
     private static final Map<Integer, String> rules = new java.util.HashMap<>();
     static {
         // add in base rules
         // called when an unexpected event happened
-        rules.put(0, "Unknown Violation");
+        rules.put(0, "UnknownViolation");
 
         // start structural rules (1)
-        rules.put(1,    "CoinbaseLengthRule");
-        rules.put(2,    "StateRootLengthRule");
-        rules.put(3,    "TxTrieLengthRule");
-        rules.put(4,    "ReceiptTrieLengthRule");
-        rules.put(5,    "LogsBloomLengthRule");
-        rules.put(6,    "DifficultyMaxLengthRule");
-        rules.put(7,    "NumberMaxSizeRule");
-        rules.put(8,    "TimestampMaxSizeRule");
-        rules.put(9,    "ExtradataMaxLengthRule");
-        rules.put(10,   "NonceMaxSizeRule");
-        rules.put(11,   "EnergyConsumedMaxSizeRule");
-        rules.put(12,   "EnergyLimitMaxSizeRule");
+        rules.put(COINBASE_LENGTH_RULE,         "CoinbaseLengthRule");
+        rules.put(STATE_ROOT_LENGTH_RULE,       "StateRootLengthRule");
+        rules.put(TX_TRIE_LENGTH_RULE,          "TxTrieLengthRule");
+        rules.put(RECEIPT_TRIE_LENGTH_RULE,     "ReceiptTrieLengthRule");
+        rules.put(LOGS_BLOOM_LENGTH_RULE,       "LogsBloomLengthRule");
+        rules.put(NUMBER_MAX_SIZE_RULE,         "NumberMaxSizeRule");
+        rules.put(TIMESTAMP_MAX_SIZE_RULE,      "TimestampMaxSizeRule");
+        rules.put(EXTRA_DATA_MAX_LENGTH_RULE,   "ExtradataMaxLengthRule");
+        rules.put(NONCE_MAX_SIZE_RULE,          "NonceMaxSizeRule");
+        rules.put(ENERGY_CONSUMED_MAX_SIZE_RULE,"EnergyConsumedMaxSizeRule");
+        rules.put(ENERGY_LIMIT_MAX_SIZE_RULE,   "EnergyLimitMaxSizeRule");
 
         // start independant validation rules (100)
-        rules.put(100,  "PoWDifficultyRule");
-        rules.put(101,  "EnergyConsumedLimitRule");
+        rules.put(POW_DIFFICULTY_RULE,          "PoWDifficultyRule");
+        rules.put(ENERGY_CONSUMED_LIMIT_RULE,   "EnergyConsumedLimitRule");
 
         // start dependant validation rules (200)
-        rules.put(200, "BlockNumberRule");
-        rules.put(201, "EnergyLimitRule");
-        rules.put(202, "TimestampRule");
+        rules.put(BLOCK_NUMBER_RULE,            "BlockNumberRule");
+        rules.put(ENERGY_LIMIT_RULE,            "EnergyLimitRule");
+        rules.put(TIMESTAMP_RULE,               "TimestampRule");
 
         // start post-execution dependant validation rules (300)
-        rules.put(300, "EnergyMatchRule");
-        rules.put(301, "ReceiptHashMatchRule");
-        rules.put(302, "LogsBloomMatchRule");
-        rules.put(303, "WorldStateMatchRule");
+        rules.put(ENERGY_MATCH_RULE,            "EnergyMatchRule");
+        rules.put(RECEIPT_HASH_MATCH_RULE,      "ReceiptHashMatchRule");
+        rules.put(LOGS_BLOOM_MATCH_RULE,        "LogsBloomMatchRule");
+        rules.put(WORLD_STATE_MATCH_RULE,       "WorldStateMatchRule");
     }
 
     private static String getRuleName(Integer ruleNumber) {
